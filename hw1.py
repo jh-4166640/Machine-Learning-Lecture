6baf5c45a6914986b03dceb35e80ed56c54aeff8
ptolemaeus <75351831+jh-4166640@users.noreply.github.com>
2025-03-21 14:57:39 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -18,9 +18,8 @@ for file_name in range(0,100,1):
     open_file_buffer=pd.read_csv(path + str(file_name) +".csv", header=None)
     data_frame = pd.DataFrame(open_file_buffer)
     temp=data_frame.values
-    if file_name % 10 == 0:
-        if file_name != 0:
-            my_row += 10
+    if file_name % 10 == 0 and file_name != 0:
+        my_row += 10
     img_vec[0][:,file_name] = temp[:,25]    
     img_vec[1][file_name,:] = temp[10,:]    
     img_vec[2][my_row:my_row+10,10*(file_name%10):10*(file_name%10) + 10] = temp[70:80,80:90]
