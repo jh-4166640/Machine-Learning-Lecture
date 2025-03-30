0f3df16d87001ece71f6ecce52b191f41df946cf
ptolemaeus <75351831+jh-4166640@users.noreply.github.com>
2025-03-31 01:46:34 +0900

    Update Lecture4.py

--- Lecture4/Lecture4.py
+++ Lecture4/Lecture4.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 1)--------------------------------------------------------------------------
-path = "C:\\Users\\USER\\lin_regression_data_01.csv"
+path = "lin_regression_data_01.csv"
 open_file=pd.read_csv(path,header=None)
 df = pd.DataFrame(open_file)
 
@@ -29,7 +29,7 @@ input_mat = data[:,0]
 #input_mat[:,1] = np.ones(numberOfData) # add to bias term
 output_mat = data[:,1] # initialize
 
-plt.scatter(input_mat,output_mat,label = "Measured Data",s=60)
+plt.scatter(input_mat,output_mat,label = "Measured Data",s=50)
 
 # 2)--------------------------------------------------------------------------
 x_avg = np.mean(input_mat)
@@ -39,13 +39,14 @@ w0_parent = np.mean((input_mat**2) - (x_avg**2))
 w0_op = w0_child/w0_parent
 w1_op = np.mean((output_mat - (w0_op*input_mat)))
 
-new_x_start = 0 #Regression start point
-new_x_end = 20  #Regression end point
-new_x_step = 0.1 #New regression step
+new_x_start = 0 #New prediction start point
+new_x_end = 20  #New prediction end point
+new_x_step = 0.1 #New prediction step
 
 new_x = np.arange(new_x_start, new_x_end, new_x_step)
 new_regression_y = w0_op*new_x + w1_op
-plt.plot(new_x,new_regression_y,'r-',label="Regression")
+
+plt.plot(new_x,new_regression_y,'r-',label="Prediction")
 
 
 plt.rc('font',size=20)
@@ -55,7 +56,7 @@ plt.title('Spring Length Graph',fontsize=24)
 plt.grid(True)
 plt.legend(fontsize=20)
 x_ticks = np.arange(new_x_start , new_x_end+2, 2) # x axis ticks interval
-y_ticks = np.arange(round(new_regression_y[0]) , round(new_regression_y[len(new_regression_y)-1]) ,1) # y axis ticks interval
+y_ticks = np.arange(round(new_regression_y[0])-1 , round(new_regression_y[len(new_regression_y)-1]) ,1) # y axis ticks interval
 plt.xlim(0,20) # x label range 0~20
 plt.xticks(x_ticks,fontsize=15)
 plt.yticks(y_ticks,fontsize=15)
@@ -63,5 +64,32 @@ plt.show()
 
 
 # 3)--------------------------------------------------------------------------
-y_regression=w0_op*input_mat + w1_op          # y^ = w0*x + w1
-mse = np.mean((y_regression - output_mat)**2) 
+y_prediction=w0_op*input_mat + w1_op          # y^ = w0*x + w1
+mse = np.mean((y_prediction - output_mat)**2) 
+
+
+w0_range = np.linspace(-2, 4,numberOfData)  # w_0 range
+w1_range = np.linspace(-4.6, 10.6, numberOfData)  # w_1 range
+w0_plot, w1_plot = np.meshgrid(w0_range, w1_range) # 1D -> 2D for broadcasting
+mse_plot = np.zeros_like(w0_plot)
+# Find the MSE for each weight
+for i in range(numberOfData):
+    for j in range(numberOfData):
+        _w0 = w0_plot[i,j]
+        _w1 = w1_plot[i,j]
+        y_pred = _w0 * input_mat + _w1
+        mse_plot[i,j] = np.mean((y_pred - output_mat)**2)
+
+fig = plt.figure(figsize=(10,7))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(w0_plot,w1_plot,mse_plot,cmap='plasma')
+plt.rc('font',size=12)
+ax.set_xlabel('W0',fontsize=15)
+ax.set_ylabel('W1', fontsize=15)
+ax.set_zlabel('MSE', fontsize=16)
+plt.title('Loss function',fontsize=24)
+plt.xticks(fontsize=11)
+plt.yticks(fontsize=11)
+ax.tick_params('z', labelsize=11)
+ax.grid(True)
+plt.show()
